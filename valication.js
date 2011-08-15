e74ab948f069e53b883dc72ccf8c82af8cad8962
Andrew Maurer <git@maurer.me>
2011-08-14 20:46:33 -0400

    Initial Code Commit

--- valication.js
+++ valication.js
@@ -1,1 +1,48 @@
-test
+
+
+
+;(function($){
+	
+	$.fn.valication = function(val, type){
+		/*
+		switch(type.toLowerCase()){
+			case 'phone' :
+				break;
+			case 'email' :
+				break;
+			default :
+				/* Check length *
+				return Boolean(val.length);
+		}
+		*/
+		var lType = type.toLowerCase();
+		if(typeof valication[lType] !== 'undefined'){
+			return validation[lType](val.toString());
+		} else {
+			return Boolean(val.length);
+		}
+
+	}
+
+	var validation = {
+		email : function(val){ 
+			var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+			return re.test(val); //test
+		},
+		phone : function(val){ 
+			var re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;  
+			return re.test(val); 
+		},
+		float : function(val){
+			return (isNaN(parseFloat(value)));
+		},
+		int : function(val){
+			return (parseInt(value) !== value);
+		},
+		integer : function(val){
+			return validation.int(val);
+		}
+
+	}
+
+})(jQuery);
